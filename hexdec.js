43a73a29ade078504427ff12da32b780746da86d
Stephanie Musinsky <sm452@duke.edu>
2017-11-27 17:33:08 -0500

    hex

--- hexdec.js
+++ hexdec.js
@@ -1,10 +1,10 @@
 exports.dec = function(hex,callback){
 	var i;
-	var[16] s;
+	var str;
 	for(i=0;i<16;i++){
 		var y = i*4;
 		var z = y+3;
-		s[i] = parseInt(hex[y:z],16);
+		str += parseInt(hex[y:z],16);
 	}
 	callback(null, s);
 }
