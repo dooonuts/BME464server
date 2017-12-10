b06e8a445913dbbf5a96a8d551a1201ec9bb0dd5
Daniel Wu <dw352934@gmail.com>
2017-12-10 07:08:44 -0500

    Changed HexDec prints

--- hexdec.js
+++ hexdec.js
@@ -7,8 +7,10 @@ exports.dec = function(hex,callback){
 	for (i=0; i+3<hex.length; i = i+4){
 		console.log(hex.substring(i,i+4));
 		str = str + parseInt(hex.substring(i,i+4),16) + " ";
-		arr[j] =parseInt(hex.substring(i,i+4),16);
+		arr[j] = parseInt(hex.substring(i,i+4),16);
 		j=j+1;
 	}
+	console.log("Printing string: " + str);
+	console.log("Printing array: " + array);
 	callback(null, str);
 }
