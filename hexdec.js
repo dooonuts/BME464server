0a3b7a61e4588a6666c5702f919d8f9a40bcafc2
Daniel Wu <dw352934@gmail.com>
2017-12-10 07:11:26 -0500

    Added declaration of array

--- hexdec.js
+++ hexdec.js
@@ -2,12 +2,12 @@ exports.dec = function(hex,callback){
 	var i;
 	var j=0;
 	var str = "";
-	var arr;
+	var arr = [];
 	console.log(hex);
 	for (i=0; i+3<hex.length; i = i+4){
 		console.log(hex.substring(i,i+4));
 		str = str + parseInt(hex.substring(i,i+4),16) + " ";
-		arr[j] = parseInt(hex.substring(i,i+4),16);
+		arr[i] = parseInt(hex.substring(i,i+4),16);
 		j=j+1;
 	}
 	console.log("Printing string: " + str);
